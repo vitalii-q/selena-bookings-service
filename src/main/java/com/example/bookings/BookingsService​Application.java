6be627fc7a8cc17122e60e456abf7415e6488c9c
vitaly <vitalicheg28@mail.ru>
2025-03-11 19:36:24 +0100

    Initial commit with project setup

---
+++ src/main/java/com/example/bookings/BookingsService​Application.java
@@ -0,0 +1,13 @@
+package com.example.bookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingsService​Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingsService​Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookings/BookingsService​ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsService​ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
